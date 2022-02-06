4fec39b1c14d2c723572fd8e2268ca193394d336
Guy de Messimy <gdemessimy@gmail.com>
2022-02-06 18:24:40 +0100

    init : typeorm and dockerfile"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MachinesModule } from './machines/machines.module';
 
 @Module({
-  imports: [MachinesModule],
+  imports: [
+    MachinesModule,
+    TypeOrmModule.forRoot({
+      logging: true,
+      type: 'mysql',
+      host: process.env.DB_HOST || 'localhost',
+      port: +process.env.MYSQL_PORT || 3306,
+      username: process.env.DATABASE_USERNAME || 'root',
+      password: process.env.DATABASE_PASSWORD || 'root',
+      database: process.env.DATABASE_NAME || 'farmingDb',
+      autoLoadEntities: true, // models will be loaded automatically
+      synchronize: true, // your entities will be synced with the database(recommended: disable in prod)
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
